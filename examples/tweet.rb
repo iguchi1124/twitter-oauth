2c3ef3e6250d661e2b1156e76ed971ddc630ef48
Shota Iguchi <e.iguchi1124@gmail.com>
2016-03-04 13:24:44 +0900

    Add tweet code

---
+++ examples/tweet.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# RubyGems
+require 'bundler'
+Bundler.require
+
+require_relative '../lib/oauth/header'
+require_relative '../lib/oauth/client'
+require 'yaml'
+
+client = OAuth::Client.new(YAML.load_file('secrets.yml'))
+client.fetch_request_token('https://api.twitter.com/oauth/request_token')
+print "次のURLで認証して下さい: https://api.twitter.com/oauth/authorize?oauth_token=#{client.token}\n"
+print "PINコードを入力して下さい: "
+client.pin = gets
+client.fetch_access_token('https://api.twitter.com/oauth/access_token')
+puts client.post('https://api.twitter.com/1.1/statuses/update.json', status: 'tweet test')
