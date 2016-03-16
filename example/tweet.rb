ab9a920f0e5f0b52ed79d3f7276a3257888c51db
Shota Iguchi <e.iguchi1124@gmail.com>
2016-03-16 23:18:23 +0900

    Remove lf code from inputs

--- example/tweet.rb
+++ example/tweet.rb
@@ -12,7 +12,6 @@ client = OAuth::Client.new(YAML.load_file('secrets.yml'))
 client.fetch_request_token('https://api.twitter.com/oauth/request_token')
 print "次のURLで認証して下さい: https://api.twitter.com/oauth/authorize?oauth_token=#{client.token}\n"
 print "PINコードを入力して下さい: "
-client.pin = gets
+client.pin = gets.chomp
 client.fetch_access_token('https://api.twitter.com/oauth/access_token')
-binding.pry
 puts client.post('https://api.twitter.com/1.1/statuses/update.json', status: 'tweet test')
