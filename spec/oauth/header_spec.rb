69c1a54aae3396a096b46d85d25fc989dd06243a
Shota Iguchi <e.iguchi1124@gmail.com>
2016-04-03 15:19:43 +0900

    Refactor header module spec

--- spec/oauth/header_spec.rb
+++ spec/oauth/header_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
+RSpec.configure do |config|
+  config.include OAuth::Header
+end
+
 describe OAuth::Header do
-  include OAuth::Header
+  let(:str) { 'str :?/=' }
 
   describe '#percent_encode' do
-    let(:str) { 'encoding string+?/=' }
     it do
       require 'cgi'
-      expect(CGI.escape str).to eq percent_encode(str)
+      expect(CGI.escape(str)).to eq(percent_encode(str))
     end
   end
 
   describe '#base64_encode' do
-    let(:str) { 'encoding string+?/=' }
     it do
       require 'base64'
-      expect(Base64.encode64 str).to eq base64_encode(str)
+      expect(Base64.encode64(str).delete("\n")).to eq(base64_encode(str))
     end
   end
 end
