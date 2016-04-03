a58d7046733e35a8b64acc4e9534bc39888f97f8
Shota Iguchi <e.iguchi1124@gmail.com>
2016-04-03 15:49:39 +0900

    Refactor

--- lib/oauth/client.rb
+++ lib/oauth/client.rb
@@ -12,22 +12,26 @@ module OAuth
     def initialize(opts = {})
       yield self if block_given?
 
-      self.consumer_key    ||= opts['consumer_key']
-      self.consumer_secret ||= opts['consumer_secret']
+      @consumer_key    ||= opts['consumer_key']
+      @consumer_secret ||= opts['consumer_secret']
 
-      self.signature_method ||= 'HMAC-SHA1'
-      self.callback         ||= opts['callback'] || 'oob'
+      @signature_method ||= 'HMAC-SHA1'
+      @callback         ||= opts['callback'] || 'oob'
+    end
+
+    def set_request_params(verb, url, opts = {})
+      @request_method = verb
+      @request_url = url
+      @options = opts
+      reset_onetime_params!
     end
 
     def get(url, opts = {})
-      @request_url = url
-      @request_method = :get
-      @options = opts.each { |k, v| opts[k] = URI.encode v.to_s }
-      reset_onetime_params!
+      set_request_params('GET', url, opts.each { |k, v| opts[k] = URI.encode(v.to_s) })
 
       uri = URI(@request_url)
       uri.query = normalized_signed_params
-      uri.query += '&' + opts.collect { |k, v| "#{k}=#{v}" }.join('&') unless opts.nil?
+      uri.query += '&' + @options.collect { |k, v| "#{k}=#{v}" }.join('&') unless @options.nil?
 
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true if uri.port == 443
@@ -39,50 +43,46 @@ module OAuth
     end
 
     def post(url, opts = {})
-      @request_url = url
-      @request_method = :post
-      @options = opts.each { |k, v| opts[k] = URI.encode v.to_s }
-      reset_onetime_params!
+      set_request_params('POST', url, opts.each { |k, v| opts[k] = URI.encode(v.to_s) })
 
       uri = URI(@request_url)
-
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true if uri.port == 443
 
       req = Net::HTTP::Post.new(uri)
       req['Authorization'] = authorization_header
-      req.body = opts.collect { |k, v| "#{k}=#{v}" }.join('&') unless opts.nil?
+      req.body = @options.collect { |k, v| "#{k}=#{v}" }.join('&') unless @options.nil?
 
       res = http.request(req)
       res.body
     end
 
-    def fetch_request_token(request_token_url, method = :post)
+    def fetch_request_token(request_token_url, method = 'POST')
       return if has_request_token?
 
-      res = send(method, request_token_url)
+      res = send(method.downcase.to_sym, request_token_url)
 
       opts = {}
       res.split('&').map { |str| str.split('=') }.each { |k, v| opts[k] = v }
 
-      self.request_token = opts['oauth_token']
-      self.request_token_secret = opts['oauth_token_secret']
+      @request_token = opts['oauth_token']
+      @request_token_secret = opts['oauth_token_secret']
 
       define_singleton_accessor(:pin) if callback == 'oob'
 
       opts
     end
 
-    def fetch_access_token(access_token_url, method = :post)
+    def fetch_access_token(access_token_url, method = 'POST')
       return if has_access_token? || !has_request_token?
 
-      res = send(method, access_token_url)
+      res = send(method.downcase.to_sym, access_token_url)
 
       opts = {}
       res.split('&').map { |str| str.split('=') }.each { |k, v| opts[k] = v }
 
-      self.access_token = opts['oauth_token']
-      self.access_token_secret = opts['oauth_token_secret']
+      @access_token = opts['oauth_token']
+      @access_token_secret = opts['oauth_token_secret']
 
       opts
     end
